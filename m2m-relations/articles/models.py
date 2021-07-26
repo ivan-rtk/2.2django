703426cb1b346b9f7de08e5e0ff0e0fc0f1c682e
ivan-rtk <ivan-rtk@mail.ru>
2021-07-26 22:45:51 +0300

    Initial commit

---
+++ m2m-relations/articles/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    topic = models.CharField(max_length=256, verbose_name='Название', default='')
+    articles = models.ManyToManyField(Article, through='ArticleScope', related_name='scopes')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.topic
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        ordering = ['-is_main', '-scope']
